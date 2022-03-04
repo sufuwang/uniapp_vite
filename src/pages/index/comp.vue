069c4cb7f0782ce76fc94e6ef801e0e84e53d08d
sufuwang <sufuwang@tencent.com>
2022-03-04 20:52:16 +0800

    fix: onMounted

---
+++ src/pages/index/comp.vue
@@ -0,0 +1,23 @@
+<template>
+  <view v-for="i in 100" :key="i">
+    {{ data }}
+  </view>
+</template>
+
+<script lang="ts">
+import { onMounted } from 'vue'
+import { onLoad } from '@dcloudio/uni-app';
+export default {
+  props: {
+    data: Number,
+  },
+  setup(props) {
+    onLoad(() => {
+      console.info('组件 onLoad: ', props.data.value, Date.now())
+    })
+    onMounted(() => {
+      console.info('组件 onMounted: ', props.data.value, Date.now())
+    });
+  }
+}
+</script>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,26 @@
-<script>
-import * as echarts from './echarts.min.js'
-console.info(echarts.init)
-console.info(echarts.setOption)
-console.info(echarts.setCanvasCreator)
+<template>
+  <Comp :data="data" />
+</template>
+
+<script lang="ts">
+import { onMounted, ref } from 'vue'
+import { onLoad } from '@dcloudio/uni-app';
+import Comp from './comp.vue'
+
+export default {
+  components: { Comp },
+  setup() {
+    const data = ref(1);
+
+    onLoad(() => {
+      data.value = 2
+      console.info('页面 onLoad: ', data.value, Date.now())
+    })
+    onMounted(() => {
+      console.info('页面 onMounted: ', data.value, Date.now())
+    });
+
+    return { data }
+  },
+};
 </script>
